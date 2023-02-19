adda72c698b658aea8abc75cf4d16b3bc3c72b19
Calvin Crosby <calvincrosby@Calvins-MacBook-Air.local>
2023-02-19 16:05:37 -0500

    added navbar parameter challenge

--- public/tuiter/NavigationSidebar/index.js
+++ public/tuiter/NavigationSidebar/index.js
@@ -1,13 +1,13 @@
 
 var items = [
-    {name: "Home", icon: "fa-home", link: ".../home.html"},
-    {name: "Explore", icon: "fa-hashtag", link: ".../explore.html"},
-    {name: "Notifications", icon: "fa-bell", link: ".../notifications.html"},
-    {name: "Messages", icon: "fa-envelope", link: ".../messages.html"},
-    {name: "Bookmarks", icon: "fa-bookmark", link: ".../bookmarks.html"},
-    {name: "Top Articles", icon: "fa-fire", link: ".../top_articles.html"},
-    {name: "Profile", icon: "fa-user", link: ".../profile.html"},
-    {name: "More", icon: "fa-ellipsis-h", link: ".../more.html"}
+    {name: "Home", icon: "fa-home", link: ".../home.html", active: true},
+    {name: "Explore", icon: "fa-hashtag", link: ".../explore.html", active: false},
+    {name: "Notifications", icon: "fa-bell", link: ".../notifications.html", active: false},
+    {name: "Messages", icon: "fa-envelope", link: ".../messages.html", active: false},
+    {name: "Bookmarks", icon: "fa-bookmark", link: ".../bookmarks.html", active: false},
+    {name: "Top Articles", icon: "fa-fire", link: ".../top_articles.html", active: false},
+    {name: "Profile", icon: "fa-user", link: ".../profile.html", active: false},
+    {name: "More", icon: "fa-ellipsis-h", link: ".../more.html", active: false}
 
 ];
 const GenerateNavItem = (item)=> {
@@ -15,7 +15,7 @@ const GenerateNavItem = (item)=> {
         <li class = "list-group-item rounded-pill mt-2 mb-2">
              <a href = ${item.link}>
                         <i class = "float-start me-3 d-inline-block fa ${item.icon} text-dark"></i>
-                        <p class = "float-start d-inline-block d-none d-xl-block m-0 text-dark">${item.name}</p>
+                        <p class = "float-start d-inline-block d-none d-xl-block m-0 ${item.active ? "text-primary fw-bold fw-": "text-dark"}">${item.name}</p>
                     </a>
         </li>
     `)
